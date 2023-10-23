446e70f0f6ff022640d667d6377654d99299e920
jogendra-b54 <jpatro.kumar@gmail.com>
2023-10-23 17:12:37 +0530

    Auto push

--- sg.tf
+++ sg.tf
@@ -3,6 +3,13 @@ resource "aws_security_group" "allows_rabbitmq" {
   description = "Allows only RabbitMQ Traffic"
   vpc_id = data.terraform_remote_state.vpc.outputs.VPC_ID
 
+  ingress {
+    description = "SSH from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [data.terraform_remote_state.vpc.outputs.VPC_CIDR , data.terraform_remote_state.vpc.outputs.DEFAULT_VPC_CIDR]     
+  } 
   
   ingress {
     description = "rabbitmq from VPC"
