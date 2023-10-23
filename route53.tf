43d0f984f052ad85ca4abdd837f99f19be6acd2c
jogendra-b54 <jpatro.kumar@gmail.com>
2023-10-23 16:35:07 +0530

    Auto push

--- route53.tf
+++ route53.tf
@@ -1,6 +1,6 @@
 resource "aws_route53_record" "rabbitmq_dns_record" {
-  zone_id = aws_route53_zone.primary.zone_id
-  name    = "www.example.com"
+  zone_id = data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTED_ZONE_ID
+  name    = "rabbitmq-${var.ENV}.${data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTED_ZONE_NAME}"
   type    = "A"
   ttl     = 300
   records = [aws_eip.lb.public_ip]
